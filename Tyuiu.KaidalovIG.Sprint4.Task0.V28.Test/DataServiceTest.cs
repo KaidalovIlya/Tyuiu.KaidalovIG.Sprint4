3dee7ff2d9207fdb8d2df903f29ef9dffe7de319
Tyuiu.KaidalovIG@outlook.com <Kaydalov.2026@mail.ru>
2023-11-05 21:56:39 +0500

    Add project files.

---
+++ Tyuiu.KaidalovIG.Sprint4.Task0.V28.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KaidalovIG.Sprint4.Task0.V28.Lib;
+
+namespace Tyuiu.KaidalovIG.Sprint4.Task0.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] a = { 1, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Assert.AreEqual(384, ds.GetMultEvenArrEl(a));
+        }
+    }
+}
---
+++ Tyuiu.KaidalovIG.Sprint4.Task0.V28.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KaidalovIG.Sprint4.Task0.V28.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KaidalovIG.Sprint4.Task0.V28.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("381ab74b-9849-40f4-bba7-2d3d32e7e610")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
