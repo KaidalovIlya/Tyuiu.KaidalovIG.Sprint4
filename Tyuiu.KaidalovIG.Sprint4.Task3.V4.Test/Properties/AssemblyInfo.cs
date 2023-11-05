db667deeb0c1055763c18909d5c1260ffda1575e
Tyuiu.KaidalovIG@outlook.com <Kaydalov.2026@mail.ru>
2023-11-05 22:27:02 +0500

    Task3

---
+++ Tyuiu.KaidalovIG.Sprint4.Task3.V4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KaidalovIG.Sprint4.Task3.V4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KaidalovIG.Sprint4.Task3.V4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f78ba3e9-803c-4386-8acf-021f50d250d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KaidalovIG.Sprint4.Task3.V4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tyuiu.KaidalovIG.Sprint4.Task3.V4.Lib;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KaidalovIG.Sprint4.Task3.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] nArray = { { 6, 5, 4, 1, 5 }, { 8, 2, 3, 4, 2 }, { 3, 7, 7, 1, 3 }, { 3, 4, 8, 1, 3 }, { 4, 3, 5, 5, 2 } };
+            int i_o = nArray.GetLength(0); int j_o = nArray.GetLength(1);
+
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Кайдалов И. Г. | СМАРТб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (статический ввод)                             *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Кайдалов Илья Григорьевич | СМАРТб-23-3                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* статическими значениями в диапазоне от 1 до 9.                          *");
+            Console.WriteLine("* Найдите сумму элементов в последнем столбце массива.                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < i_o; i++)
+            {
+                for (int j = 0; j < j_o; j++)
+                {
+                    Console.Write(" " + nArray[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(nArray);
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+}
